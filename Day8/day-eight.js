008dbabb2e437deab094025b5961907ddfa1e527
Samyak Shah <samyakshah3008@gmail.com>
2024-11-19 16:05:59 +0530

    feat: candy problem solve with two pass approach

--- Day8/day-eight.js
+++ Day8/day-eight.js
@@ -2,65 +2,48 @@
  * @param {number[]} ratings
  * @return {number}
  */
-var candyBruteForce = function (ratings) {
-  let distributedCandiesArray = [];
-  for (let i = 0; i < ratings.length; i++) {
-    if (ratings[i] == 0) {
-      distributedCandiesArray.push(1);
-      continue;
-    }
-    if (i == ratings.length - 1) {
-      if (ratings[i - 1] >= ratings[i]) {
-        distributedCandiesArray.push(1);
-      } else {
-        distributedCandiesArray.push(2);
-      }
-    } else if (i == 0) {
-      if (ratings[i] > ratings[i + 1]) {
-        distributedCandiesArray.push(2);
-      } else {
-        distributedCandiesArray.push(1);
-      }
-    } else {
-      if (ratings[i] > ratings[i + 1] || ratings[i] > ratings[i - 1]) {
-        distributedCandiesArray.push(2);
-      } else {
-        distributedCandiesArray.push(1);
-      }
-    }
-  }
 
-  let sum = distributedCandiesArray.reduce((acc, curr) => acc + curr, 0);
-  return sum;
-};
+function candyBruteForce(ratings) {
+  const newArrayLeft = [];
+  newArrayLeft[0] = 1;
+  newArrayLeft[ratings.length - 1] = 1;
 
-function candyOptimized(ratings) {
-  const n = ratings?.length;
-  const distributedCandiesArray = Array(n).fill(1);
+  // left pass;
 
-  // left to right pass;
-
-  for (let i = 1; i < n; i++) {
+  for (let i = 1; i < ratings.length - 1; i++) {
     if (ratings[i] > ratings[i - 1]) {
-      distributedCandiesArray[i] = distributedCandiesArray[i - 1] + 1;
+      newArrayLeft[i] = newArrayLeft[i - 1] + 1;
+    } else {
+      newArrayLeft[i] = 1;
     }
   }
 
-  // right to left pass;
-  for (let i = n - 2; i >= 0; i--) {
+  const newArrayRight = [];
+  newArrayRight[0] = 1;
+  newArrayRight[ratings.length - 1] = 1;
+
+  // right pass;
+
+  for (let i = ratings.length - 2; i >= 0; i--) {
     if (ratings[i] > ratings[i + 1]) {
-      distributedCandiesArray[i] = Math.max(
-        distributedCandiesArray[i],
-        distributedCandiesArray[i + 1] + 1
-      );
+      newArrayRight[i] = newArrayRight[i + 1] + 1;
+    } else {
+      newArrayRight[i] = 1;
     }
   }
 
-  return distributedCandiesArray.reduce((acc, curr) => acc + curr, 0);
+  let finalArray = [];
+
+  for (let i = 0; i < ratings.length; i++) {
+    finalArray.push(Math.max(newArrayLeft[i], newArrayRight[i]));
+  }
+
+  return finalArray.reduce((acc, curr) => acc + curr, 0);
 }
 
 let ratings = [1, 0, 2];
 // let ratings = [1, 2, 2];
 ratings = [1, 2, 87, 87, 87, 2, 1];
+ratings = [0, 2, 4, 3, 2, 1, 1, 3, 5, 6, 4, 0, 0];
 
-console.log(candyOptimized(ratings));
+console.log(candyBruteForce(ratings));
